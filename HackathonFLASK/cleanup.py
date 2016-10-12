0ba03bf4fceee55b620d30928dba7089f2af7b63
Anthony Scott <ascott329@gmail.com>
2016-10-12 09:40:46 -0500

    temp dir is recreated after delete

--- HackathonFLASK/cleanup.py
+++ HackathonFLASK/cleanup.py
@@ -4,5 +4,12 @@ import os
 dir_name = os.path.dirname(os.path.realpath(__file__)) + '/static/temp'
 
 shutil.rmtree(dir_name)
+print("TEMP DIRECTORY DELETED")
 
-print("TEMP DIRECTORY EMPTIED")
+
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
+
+print("TEMP DIRECTORY RE-CREATED")
+
+
